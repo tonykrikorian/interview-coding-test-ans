31b51320a4c0ea346b63cdf94c788cf552162cfb
Tony Krikorian <tony.krikorian@outlook.com>
2021-07-07 18:44:28 -0400

    Agregar clase Product
    Esta es una clase base de la que extienden todos los productos, se
    agrega con el objetivo de implementar de hacer el codigo polimorfico.

---
+++ src/Products/Product.ts
@@ -0,0 +1,13 @@
+import {ProductsEnum} from './ProductsEnum'
+export default class Product
+{
+    constructor(public name:string, public sellIn:number, public price:number) {}
+
+    calculatePrice():Product{
+        if(this.sellIn <=0) this.price-=2
+        if(this.sellIn>0) --this.price
+        if(this.price<=0) this.price=0
+        --this.sellIn;
+        return this
+    }
+}
